30c868bc5c617fe9820ce79c330abe5dd6e38040
Tao Huang <countcainht@gmail.com>
2018-05-28 17:13:07 -0400

    update shell

--- base/setup.zsh
+++
@@ -1,7 +0,0 @@
-rm -rf "${ZDOTDIR:-$HOME}/.zprezto"
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-
-rm -rf "$HOME/.vim"
-mkdir -p "$HOME/.vim"
-mkdir -p "$HOME/.vim/bundle"
-git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
